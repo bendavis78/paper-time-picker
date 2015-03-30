08daee76bf4eda3e4e849ac04349944d44398706
Ben Davis <bendavis78@gmail.com>
2015-03-30 16:48:27 -0500

    Changed to allow dragging within clock to select nearest number

--- paper-time-picker.js
+++ paper-time-picker.js
@@ -12,7 +12,7 @@
     publish: {
       hour: 0,
       minute: 0,
-      responsiveWidth: '640px',
+      responsiveWidth: '450px',
       narrow: {type: 'boolean', value: false, reflect: true},
       isTouch: {type: 'boolean', value: false, reflect: true}
     },
@@ -35,16 +35,13 @@
       });
     },
     _initClock: function() {
-      var hoursClock = {}
-      var minutesClock = {}
-
-      hoursClock.type = CLOCK_TYPE_HOURS;
-      minutesClock.type = CLOCK_TYPE_MINUTES;
-      hoursClock.numbers = [];
-      minutesClock.numbers = [];
+      var clock = {
+        hours: [],
+        minutes: []
+      };
 
       for (var i=0; i<12; i++) {
-        hoursClock.numbers.push({
+        clock.hours.push({
           number: i,
           numberDisplay: i == 0 ? 12: i,
           visible: true
@@ -52,48 +49,46 @@
       }
 
       for (var i=0; i<60; i++) {
-        minutesClock.numbers.push({
+        clock.minutes.push({
           number: i,
           numberDisplay: ('0' + i).substr(-2, 2),
           visible: i % 5 == 0
         });
       }
 
-      hoursClock.selectPeriod = this.selectPeriod.bind(this);
-      minutesClock.selectPeriod = this.selectPeriod.bind(this);
-
-      this.$.hoursClock.model = hoursClock;
-      this.$.minutesClock.model = minutesClock;
+      this.clock = clock;
+      this.$.hoursClock.model = {
+        type: CLOCK_TYPE_HOURS,
+        numbers: clock.hours,
+        clock: clock
+      };
+      this.$.minutesClock.model = {
+        type: CLOCK_TYPE_MINUTES,
+        numbers: clock.minutes,
+        clock: clock
+      };
     },
     _updateClockMetrics: function() {
-      var hoursClock = this.$.hoursClock.model;
-      var minutesClock = this.$.minutesClock.model;
+      var clock = this.clock;
+      var clockFace = this.$.clockFace;
       var radius = this.$.clockFace.offsetWidth / 2;
-      var pointerLarge = radius * (POINTER_LARGE / 100);
-      var pointerSmall = radius * (POINTER_SMALL / 100);
-      var periodChooser = radius * (PERIOD_CHOOSER / 100);
-      var padding = radius * (CLOCK_FACE_PADDING / 100);
-
-      hoursClock.periodChooser = periodChooser;
-      minutesClock.periodChooser = periodChooser;
-      hoursClock.radius = radius;
-      hoursClock.pointerLarge = pointerLarge;
-      hoursClock.pointerSmall = 0;
-
-      minutesClock.radius = radius;
-      minutesClock.pointerLarge = pointerLarge;
-      minutesClock.pointerSmall = pointerSmall;
-
-      this._positionClockPoints(hoursClock, pointerLarge, padding);
-      this._positionClockPoints(minutesClock, pointerLarge, padding);
+      clock.radius = radius;
+      clock.rect = clockFace.getBoundingClientRect();
+      clock.pointerLarge = radius * (POINTER_LARGE / 100);
+      clock.pointerSmall = radius * (POINTER_SMALL / 100);
+      clock.periodChooser = radius * (PERIOD_CHOOSER / 100);
+      clock.padding = radius * (CLOCK_FACE_PADDING / 100);
+      this._positionClockPoints(clock.hours);
+      this._positionClockPoints(clock.minutes);
     },
-    _positionClockPoints: function(model, size, padding) {
-      var points = model.numbers.length;
+    _positionClockPoints: function(numbers) {
+      var points = numbers.length;
       var angle = (360 / points) * (Math.PI / 180);
-      var r = model.radius;
-      var r1 = r - padding;
-      var r2 = r1 - (size / 2);
-      var r3 = r1 - size;
+      var clock = this.clock;
+      var r = clock.radius;
+      var r1 = r - clock.padding;
+      var r2 = r1 - (clock.pointerLarge / 2);
+      var r3 = r1 - clock.pointerLarge;
       var sin, cos, a, aA, aB;
       for (var i=0; i<points; i++) {
         a = angle * i;
@@ -106,22 +101,15 @@
         cosA = Math.cos(aA);
         sinB = Math.sin(aB);
         cosB = Math.cos(aB);
-
-        // center
-        model.numbers[i].x = r + (sin * r2);
-        model.numbers[i].y = r - (cos * r2);
-
-        // inner edge of larger circle
-        model.numbers[i].edge = {x: r + (sin * r3), y: r - (cos * r3)};
-
-        model.numbers[i].t1 = {x: r + (sinA * r), y: r - (cosA * r)};
-        model.numbers[i].t2 = {x: r + (sinB * r), y: r - (cosB * r)};
-        model.numbers[i].t3 = {x: r + (sinB * r3), y: r - (cosB * r3)};
-        model.numbers[i].t4 = {x: r + (sinA * r3), y: r - (cosA * r3)};
+        // position of center of selector
+        numbers[i].x = r + (sin * r2);
+        numbers[i].y = r - (cos * r2);
+        // end-point on line from radius to edge of selector
+        numbers[i].edge = {x: r + (sin * r3), y: r - (cos * r3)};
       }
-      model.r1 = r1;
-      model.r2 = r2;
-      model.r3 = r3;
+      clock.r1 = r1;
+      clock.r2 = r2;
+      clock.r3 = r3;
     },
     selectClockNumber: function(e, n, el) {
       var num = parseInt(el.getAttribute('value'));
@@ -138,6 +126,43 @@
       this.page = 'minute';
       this.vibrate();
     },
+    startClockSelect: function(e) {
+      this._startClockSelect = true;
+      var clock = this.clock;
+      var page = this.page;
+      var r = clock.radius;
+      var x = e.clientX - clock.rect.left - r;
+      var y = e.clientY - clock.rect.top - r;
+      var distance = Math.abs(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+      if (distance < clock.r3 || distance > r) {
+        return;
+      }
+      var points = this.page == 'minute' ? 60 : 12;
+      var interval = (360 / points) * (Math.PI / 180);
+
+      // find angle, rotate -90deg, then convert to a full circle value;
+      var theta = Math.atan(y / x)
+      theta = (Math.PI / 2) + (x < 0 ? theta + Math.PI : theta);
+
+      // find the nearest value
+      value = Math.round(theta / interval);
+      
+      // set to zero if max (eg, 12 = 0);
+      value = (value == points) ? 0 : value;
+
+      // set the hour/minute property depending on current page
+      if (this[page] != value) {
+        if (page == 'hour' && this.period == 'PM') {
+            value += 12;
+        }
+        this[this.page] = value;
+      }
+    },
+    finishClockSelect: function() {
+      if (this.page == 'hour') {
+        this.page = 'minute';
+      }
+    },
     pageChanged: function() {
       this.$.timePartSelector.selected = this.page;
       this.$.clockFace.selected = this.page;
@@ -147,9 +172,10 @@
       this.period = ['AM', 'PM'][Math.floor(this.hour / 12) % 2]
       var model = this.$.hoursClock.model;
       var idx = this.period == 'AM' ? this.hour : this.hour - 12;
-      console.log(idx);
-      console.log(model.numbers[idx]);
       model.selected = model.numbers[idx]
+    },
+    periodChanged: function() {
+      this.clock.period = this.period;
     },
     minuteChanged: function() {
       var model = this.$.minutesClock.model;
@@ -167,8 +193,8 @@
         this.hour -= 12;
       }
     },
-    selectPeriod: function(period) {
-      console.log('tapped');
+    selectPeriod: function(e, n, el) {
+      var period = el.getAttribute('value');
       this.async(function() {
         if (period == 'AM' && this.period == 'PM') {
           this.hour -= 12;
@@ -179,7 +205,7 @@
     },
     vibrate: function() {
       if (navigator.vibrate) {
-        navigator.vibrate(300);
+        navigator.vibrate(100);
       }
     }
   });
