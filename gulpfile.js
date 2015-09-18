7e37f6e97f09df50eeb0ec2be989b82a841b1518
Ben Davis <bendavis78@gmail.com>
2015-09-18 15:34:02 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gulp = require('gulp-param')(require('gulp'), process.argv);
+var gulp = require('gulp');
 var add = require('gulp-add');
 var del = require('del');
 var browserSync = require('browser-sync').create();
@@ -71,7 +71,7 @@ gulp.task('release:minor', _bump.bind(null, 'minor'));
 gulp.task('release:major', _bump.bind(null, 'major'));
 
 // Watch Files For Changes & Reload
-gulp.task('serve', ['bower'], function (port) {
+gulp.task('serve', ['bower'], function () {
   var pkgRoot = '/' + getPackage().name;
   var opts = {
     notify: false,
@@ -89,29 +89,16 @@ gulp.task('serve', ['bower'], function (port) {
       routes: {}
     }
   };
-
-  if (port) {
-    port = parseInt(port);
-    opts.port = port;
-    opts.ui = {port: port + 1};
-  }
-
   opts.server.routes[pkgRoot] = '.';
   browserSync.init(opts);
 
   gulp.watch(['**/*.html']).on('change', reload);
   gulp.watch(['**/*.css']).on('change', reload);
-  gulp.watch(['bower.json']).on('change', function() {
-    gulp.start('bower:reload');
-  });
+  gulp.watch(['bower.json']).on('change', 'bower:reload');
 });
 
 gulp.task('gh-pages', function() {
   var pkgName = getPackage().name;
-
-  // delete remote gh-pages branch
-  git.push('origin', ':gh-pages');
-
   return merge(
     gulp.src(gitFiles())
       .pipe(copy('.tmp/' + pkgName)),
